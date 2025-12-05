532476b982f41bc4304e26599706474f2ff147c2
goatzygames <7hamsterman7@gmail.com>
2025-12-05 21:29:25 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -71,3 +71,20 @@ thumbs.forEach((thumb, index) => {
     thumb.classList.add('active');
   });
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(link => 
+{
+  link.addEventListener("click", function(e)
+  {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+    if (!target) return;
+
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  });
+});
+
